c561073490f12a6ab60d38a32d2636807942f8d1
Jeyther <jeyther13@gmail.com>
2022-05-05 17:40:24 -0500

    responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const style = {
+
+        transform: isOpen?"translateY(0%)":"translateY(-150%)"
+
+
+    }
+
     return (
 
         //navbar component
@@ -10,19 +20,40 @@ const NavBar = () => {
                 Coffee<span className='navbar__brand-bold'>Land</span>
             </NavLink>
 
-            <div className='navbar__menu'>
-                <NavLink to="/" className='navbar__menu__link'>
-                    Home
-                </NavLink>
-                <NavLink to="/AboutUs" className='navbar__menu__link'>
-                    About Us
-                </NavLink>
-                <NavLink to="/Courses" className='navbar__menu__link'>
-                    Courses
-                </NavLink>
-                <NavLink to="/Contact" className='navbar__menu__link'>
-                    Contact
-                </NavLink>
+            {isOpen ?
+            (
+            <span class="material-symbols-outlined navbar__button" onClick={() => setIsOpen(!isOpen)}>
+                close
+            </span>
+            ) : (
+            <span class="material-symbols-outlined navbar__button" onClick={() => setIsOpen(!isOpen)}>
+                menu
+            </span>
+            )}
+
+
+            {/* <div className={`navbar__menu ${isOpen ? 'show' : ''}`}> */}
+            <div className={`navbar__menu`} style={style} onClick={() => setIsOpen(false)}>
+                <div className='navbar__menu__item'>
+                    <NavLink to="/" className='navbar__menu__item__link'>
+                        Home
+                    </NavLink>
+                </div>
+                <div className='navbar__menu__item'>
+                    <NavLink to="/AboutUs" className='navbar__menu__item__link'>
+                        About Us
+                    </NavLink>
+                </div>
+                <div className='navbar__menu__item'>
+                    <NavLink to="/Courses" className='navbar__menu__item__link'>
+                        Courses
+                    </NavLink>
+                </div>
+                <div className='navbar__menu__item'>
+                    <NavLink to="/Contact" className='navbar__menu__item__link'>
+                        Contact
+                    </NavLink>
+                </div>
             </div>
         </nav>
     )
